8df4bb5edb2d71d2e1b8c17c568c7619f43e4b4c
Brian Meeker <curiouscurmudgeon.com>
2010-07-07 18:17:19 +0000

    refs #7303  * Fixed a typo that was causing elements to be added incorrectly to the page in IE. I can't believe it even worked in Firefox.  * Fixed a bug that was allowing the form to submit even if a resolution was required.  * Converted all tabs to spaces.  * Ran it through jslint and made recommended fixes.

--- batchmod/htdocs/js/batchmod.js
+++ batchmod/htdocs/js/batchmod.js
@@ -3,48 +3,52 @@
 jQuery(document).ready(function($){
     // Convenience function for creating a <label>
     function createLabel(text, htmlFor) {
-      var label = $($.htmlFormat("<label>$1</label>", text));
-      if (htmlFor)
-        label.attr("for", htmlFor).addClass("control");
-      return label;
+        var label = $($.htmlFormat("<label>$1</label>", text));
+        if (htmlFor) {
+            label.attr("for", htmlFor).addClass("control");
+        }
+        return label;
     }
-	
-	// Convenience function for creating an <input type="text">
+    
+    // Convenience function for creating an <input type="text">
     function createText(name, size) {
-      return $($.htmlFormat('<input type="text" name="$1" size="$2">', 
-                            name, size));
+        return $($.htmlFormat('<input type="text" name="$1" size="$2">', 
+                                name, size));
     }
     
     // Convenience function for creating an <input type="checkbox">
     function createCheckbox(name, value, id) {
-      return $($.htmlFormat('<input type="checkbox" id="$1" name="$2"' +
-                            ' value="$3">', id, name, value));
+        return $($.htmlFormat('<input type="checkbox" id="$1" name="$2"' +
+                                ' value="$3">', id, name, value));
     }
     
     // Convenience function for creating an <input type="radio">
     function createRadio(name, value, id) {
-      // Workaround for IE, otherwise the radio buttons are not selectable
-      return $($.htmlFormat('<input type="radio" id="$1" name="$2"' +
-                            ' value="$3">', id, name, value));
+        // Workaround for IE, otherwise the radio buttons are not selectable
+        return $($.htmlFormat('<input type="radio" id="$1" name="$2"' +
+                                ' value="$3">', id, name, value));
     }
   
     // Convenience function for creating a <select>
     function createSelect(name, options, optional) {
         var e = $($.htmlFormat('<select id="$1" name="$1">', name));
-        if (optional)
+        if (optional) {
             $("<option>").appendTo(e);
+        }
         for (var i = 0; i < options.length; i++) {
             var opt = options[i], v = opt, t = opt;
-            if (typeof opt == "object") 
-                v = opt.value, t = opt.text;
+            if (typeof opt == "object") {
+                v = opt.value;
+                t = opt.text;
+            }
             $($.htmlFormat('<option value="$1">$2</option>', v, t)).appendTo(e);
         }
         return e;
     }
-	
-	//Create the appropriate input for the property.
+    
+    //Create the appropriate input for the property.
     function createInput(inputName, property){
-        var td = $('<td class="batchmod_property>')
+        var td = $('<td class="batchmod_property">');
         switch(property.type){
             case 'select':
                 td.append(createSelect(inputName, property.options, true));
@@ -74,15 +78,15 @@ jQuery(document).ready(function($){
         }
         return td;
     }
-	
-	function getInputName(propertyName){
+    
+    function getInputName(propertyName){
         return 'batchmod_value_' + propertyName;
     }
 
-	function getDisabledOptions(){
-		return $("#add_batchmod_field option:disabled");
-	}
-	
+    function getDisabledOptions(){
+        return $("#add_batchmod_field option:disabled");
+    }
+    
     //Add a new column with checkboxes for each ticket.
     //Selecting a ticket marks it for inclusion in the batch. 
     $("table.listing tr td.id").each(function() {
@@ -102,44 +106,45 @@ jQuery(document).ready(function($){
   
     //At least one ticket must be selected to submit the batch.
     $("form#batchmod_form").submit(function() {
-		//First remove all existing validation messages.
-		$(".batchmod_required").detach()
-		
-		var valid = true;
+        //First remove all existing validation messages.
+        //$(".batchmod_required").detach()
+        
+        var valid = true;
         var selectedTix=[];    
         $("input[name=selectedTicket]:checked").each( function(){ selectedTix.push(this.value);} ); 
         $("input[name=selectedTickets]").val(selectedTix);
-		
-		//At least one ticket must be selected.
-        if(selectedTix.length == 0){
+        
+        //At least one ticket must be selected.
+        if(selectedTix.length === 0){
             $("#batchmod_submit").after('<span class="batchmod_required">You must select at least one ticket.</span>');
             valid = false;
         }
-		
-		//Check that each radio property has something selected.
-		getDisabledOptions().each(function(){
-		    var propertyName = $(this).val();
-		    if(properties[propertyName].type == "radio"){
-		        var isChecked = false;
-		        var inputName = getInputName(propertyName);
-		        $("[name=" + inputName + "]").each(function(){
-		            isChecked = isChecked || $(this).is(':checked');
-		        })
-		        if(!isChecked){
-					//Select the last label in the row to add the error message
-		            $("[name=" + inputName + "] ~ label:last")
-						.after('<span class="batchmod_required">Required</span>')
-					valid = false;
-		        }
-		    }
-		})
-		
-		//If the status is set to closed, a resolution must be selected.
-		if($("#batchmod_value_status_closed").checked() && $("#batchmod_resolution").length < 1){
-			$("[name=batchmod_value_status] ~ label:last").after('<span class="batchmod_required">Resolution Required</span>')
-		}
-		
-		return valid;
+        
+        //Check that each radio property has something selected.
+        getDisabledOptions().each(function(){
+            var propertyName = $(this).val();
+            if(properties[propertyName].type == "radio"){
+                var isChecked = false;
+                var inputName = getInputName(propertyName);
+                $("[name=" + inputName + "]").each(function(){
+                    isChecked = isChecked || $(this).is(':checked');
+                });
+                if(!isChecked){
+                    //Select the last label in the row to add the error message
+                    $("[name=" + inputName + "] ~ label:last")
+                        .after('<span class="batchmod_required">Required</span>');
+                    valid = false;
+                }
+            }
+        });
+        
+        //If the status is set to closed, a resolution must be selected.
+        if($("#batchmod_value_status_closed").checked() && $("#batchmod_resolution").length < 1){
+            $("[name=batchmod_value_status] ~ label:last").after('<span class="batchmod_required">Resolution Required</span>');
+            valid = false;
+        }
+        
+        return valid;
     });
   
     //Collapse the form by default
@@ -147,11 +152,12 @@ jQuery(document).ready(function($){
   
     //Add the new batch modify field when the user selects one from the dropdown.
     $("#add_batchmod_field").change(function() {
-        if (this.selectedIndex < 1)
+        if (this.selectedIndex < 1){
             return;
+        }
         
-		//Trac has a properties object that has information about each property that filters
-		//could be built with. We use it hear to add batchmod fields.
+        //Trac has a properties object that has information about each property that filters
+        //could be built with. We use it hear to add batchmod fields.
         var propertyName = this.options[this.selectedIndex].value;
         var property = properties[propertyName];
         
@@ -162,9 +168,9 @@ jQuery(document).ready(function($){
             .append($('<div class="inlinebuttons">')
                 .append($('<input type="button" value="&ndash;">')
                     .click(function() { 
-						$('#batchmod_' + propertyName).remove();
-        				$($.htmlFormat("#add_batchmod_field option[value='$1']", propertyName)).enable(); 
-					})
+                        $('#batchmod_' + propertyName).remove();
+                        $($.htmlFormat("#add_batchmod_field option[value='$1']", propertyName)).enable(); 
+                    })
                 )
             )
         );
@@ -177,20 +183,20 @@ jQuery(document).ready(function($){
         // Add the input element.
         tr.append(createInput(getInputName(propertyName), property));
         
-		//New rows are added in the same order as listed in the dropdown. This is the same behavior as the filters.
-		var insertionPoint = null;
-		getDisabledOptions().each(function(){
-			if(insertionPoint == null && $(this).val() > propertyName){
-				insertionPoint = $("#batchmod_" + $(this).val());
-			}
-			
-		})
-		if (insertionPoint == null) {
-			insertionPoint = $("#batchmod_comment");
-		}
+        //New rows are added in the same order as listed in the dropdown. This is the same behavior as the filters.
+        var insertionPoint = null;
+        getDisabledOptions().each(function(){
+            if(insertionPoint === null && $(this).val() > propertyName){
+                insertionPoint = $("#batchmod_" + $(this).val());
+            }
+            
+        });
+        if (insertionPoint === null) {
+            insertionPoint = $("#batchmod_comment");
+        }
         insertionPoint.before(tr);
-		
+        
         //Disable each element from the option list when it is selected.
-        this.options[this.selectedIndex].disabled = 'disabled'
-	});
+        this.options[this.selectedIndex].disabled = 'disabled';
+    });
 });
